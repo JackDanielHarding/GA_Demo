273ba3d2615ce2d923f33fa1dbdf04b01916077e
Callum Shipton <callumshipton@msn.com>
2017-09-21 14:26:43 +0100

    Added GLFW, OpenGL and stb (image/font loader)

--- GA_Demo/src/main/Main.java
+++ GA_Demo/src/main/Main.java
@@ -1,39 +1,57 @@
 package main;
-import java.util.Random;
 
+import java.util.Random;
+import static org.lwjgl.glfw.GLFW.*;
+
+import Display.Window;
 import entities.Entity;
 import entities.Vector2i;
 import map.TileMap;
 import map.TileType;
 
 public class Main {
-	
+
 	private static final int NUM_ENTITIES = 10;
-	private static final int MAP_SIZE     = 10;
-	private static final int NUM_FOOD     = 5;
-	
+	private static final int MAP_SIZE = 10;
+	private static final int NUM_FOOD = 5;
+
 	private Entity[] entities;
 	private int generation = 1;
 	private Entity bestEntity;
 	
-	public void run(){
-		TileMap map = new TileMap(MAP_SIZE);
+	TileMap map;
+	
+	private static final int width = 600;
+	private static final int height = 600;
+	
+	Window w;
+
+	public void run() {
+		setup();
+		while(!glfwWindowShouldClose(w.getHandle())) loop();
+	}
+
+	public void setup() {
 		
+		w = new Window(width,height);
+
+		map = new TileMap(MAP_SIZE);
+
 		entities = new Entity[NUM_ENTITIES];
-		
+
 		Random rand = new Random();
 		int x;
 		int y;
-		
-		for(int i = 0; i < entities.length; i++){
+
+		for (int i = 0; i < entities.length; i++) {
 			do {
 				x = rand.nextInt(MAP_SIZE);
 				y = rand.nextInt(MAP_SIZE);
 			} while (!map.getTile(x, y).equals(TileType.EMPTY));
 			entities[i] = new Entity(new Vector2i(x, y));
 		}
-		
-		for (int i = 0; i < NUM_FOOD; i++){
+
+		for (int i = 0; i < NUM_FOOD; i++) {
 			do {
 				x = rand.nextInt(MAP_SIZE);
 				y = rand.nextInt(MAP_SIZE);
@@ -41,20 +59,26 @@ public class Main {
 			map.setTile(x, y, TileType.FOOD);
 		}
 		
-		for(Entity entity : entities){
+		for (Entity entity : entities) {
 			Vector2i entityPosition = entity.getPosition();
 			map.setTile(entityPosition.getX(), entityPosition.getY(), TileType.EMPTY);
-			if(!entity.isDead()){
+			if (!entity.isDead()) {
 				Vector2i entityMovement = entity.getMovement(map);
 				map.setTile(entityMovement.getX(), entityMovement.getY(), TileType.ENTITY);
 			}
-			if(entity.getFitness() > bestEntity.getFitness()){
+			if (bestEntity == null || entity.getFitness() > bestEntity.getFitness()) {
 				bestEntity = entity;
 			}
 		}
 	}
-	
-	public static void main(String[] args){
+
+	public void loop() {
+		
+		w.refresh();
+
+	}
+
+	public static void main(String[] args) {
 		Main main = new Main();
 		main.run();
 	}
